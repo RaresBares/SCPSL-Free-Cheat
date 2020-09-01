47041fa28b3aab69f8e0c0cdecbabe555aed7d67
Rares <>
2020-09-01 18:31:36 +0200

    Initial commit

---
+++ SCP-Universal-Cheat/Properties/Loader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace SCP_Universal_Cheat
+{
+    public class Loader
+    {
+        public static void Init()
+        {
+            Loader.Load = new GameObject();
+            Loader.Load.AddComponent<SUC>();
+            UnityEngine.Object.DontDestroyOnLoad(Loader.Load);
+        }
+ 
+        private static GameObject Load;
+    }
+
+    public class SUC : MonoBehaviour
+    {
+    }
+}
